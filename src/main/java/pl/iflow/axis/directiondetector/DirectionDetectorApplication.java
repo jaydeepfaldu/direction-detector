5a9b3c63861dace28819e8c6f99fa954c1819d4e
jaydeepfaldu <jaydeepfaldu1989@gmail.com>
2019-01-21 21:08:27 +0100

    initialization

---
+++ src/main/java/pl/iflow/axis/directiondetector/DirectionDetectorApplication.java
@@ -0,0 +1,14 @@
+package pl.iflow.axis.directiondetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectionDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectionDetectorApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/pl/iflow/axis/directiondetector/ServletInitializer.java
@@ -0,0 +1,14 @@
+package pl.iflow.axis.directiondetector;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DirectionDetectorApplication.class);
+    }
+
+}
+
